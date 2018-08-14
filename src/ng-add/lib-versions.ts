db6a5cec464031b9b204f62b4e1a7b1668876eb3
Zack Yang <zack9433@gmail.com>
2018-08-14 11:47:29 +0800

    update tp ui version

--- src/ng-add/lib-versions.ts
+++ src/ng-add/lib-versions.ts
@@ -1,5 +1,5 @@
 export const angularVersion = '^6.1.1';
-export const ngxFormlyVersion = '^5.0.0-beta.0';
+export const ngxFormlyVersion = '^5.0.0-beta.2';
 export const ngxTranslateVersion = '^10.0.2';
 export const ngrxStoreVersion = '^6.0.1';
-export const tpUiVersion = '^0.0.7';
+export const tpUiVersion = '^0.0.8';
